cc81793424ab96d043fe949e74592513512ee629
Farhad Ali Irinel GUL <farhad_ali.gul@stud.acs.upb.ro>
2023-05-07 21:55:30 +0300

    Solved ex4

--- ex4/ex4.py
+++ ex4/ex4.py
@@ -2,3 +2,37 @@
 # O intrare este reprezentata de o instanta a clasei Person (pe care trebuie sa o creati) 
 # care are minim 3 atribute (nume, varsta, email). Cheia unica este representata de adresa de mail. 
 
+class Person:
+    def __init__(self, name, age, email):
+        self.name = name
+        self.age = age
+        self.email = email
+        
+    def __str__(self):
+        return f"{self.name} {self.age} {self.email}"
+
+# Generate random number between 20 and 60
+import random
+def rand():
+    return random.randint(20, 60)
+
+# Database
+persons = []
+
+
+def add_pearsons():
+    for i in range(501):
+        persons.append(Person(f"Name{i}", rand(), f"email{i}@ocw.pub.ro"))
+
+def get_persons_by_email(email):
+    for person in persons:
+        if person.email.lower() == email.lower():
+            return person
+    raise ValueError("No person with this email")
+
+# Adaugam persoanele
+add_pearsons()
+
+# Test function - get_persons_by_email
+print(get_persons_by_email("email1@ocw.pub.ro").name)
+
