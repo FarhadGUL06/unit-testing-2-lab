0551eb94fe61fc70c0a6c1dcf64d6d333e64d21f
Farhad Ali Irinel GUL <farhad_ali.gul@stud.acs.upb.ro>
2023-05-07 21:31:44 +0300

    Solved ex1

--- ex1/ex1.py
+++ ex1/ex1.py
@@ -1,8 +1,17 @@
 import datetime
 from unittest.mock import Mock
 
+bisect = datetime.datetime(2020, 1, 1)
+not_bisect = datetime.datetime(2023, 1, 1)
+
+datetime = Mock()
 
 def is_leap_year():
     today = datetime.datetime.today()
     return today.year % 400 == 0 or (today.year % 4 == 0 and today.year % 100 != 0)
 
+datetime.datetime.today.return_value = bisect
+assert is_leap_year()
+datetime.datetime.today.return_value = not_bisect
+assert not is_leap_year()
+ 
